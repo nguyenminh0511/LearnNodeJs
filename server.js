406a3ec5a91037efcd88cfa6ec0bab422ff84fec
nguyenminh0511 <65353665+nguyenminh0511@users.noreply.github.com>
2022-03-17 15:04:40 +0700

    use jwt to check login

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const PORT = process.env.PORT;
 
 const accountRouter = require("./routers/account");
+const AccountModel = require("./models/account");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -16,8 +20,50 @@ app.get('/', (req, res) => {
     res.json("Hello world!");
 })
 
-app.get('/home', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+const checkLogin = (req, res, next) => {
+    try {
+        let check = jwt.verify(req.cookies.token, "pass");
+        if (check) {
+            next();
+        }
+    } catch(err) {
+        res.redirect('/login');
+    }
+}
+
+app.get('/home', checkLogin, (req, res, next) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+})
+
+app.get('/login', (req, res, next) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+})
+
+app.post('/login', (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    AccountModel.findOne({
+        username: username,
+        password: password
+    })
+    .then(data => {
+        if (data) {
+            let token = jwt.sign({
+                _id: data._id
+            }, "pass");
+            res.json({
+                message: 'success',
+                token: token
+            });
+        } else {
+            return res.redirect('/login');
+        }
+        
+    })
+    .catch(err => {
+        res.json("error");
+    })
 })
 
 app.use('/api/account/', accountRouter);
